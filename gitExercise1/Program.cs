a5cca10a236938278726056664305ef66fdbe653
amos-parrish <aparrish0314@gmail.com>
2016-11-03 20:39:17 -0500

    Added WriteLine statements.

---
+++ gitExercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello from Main!");
+            Console.WriteLine("Does this thing work?");
+            Console.WriteLine("I really hope so.");
+            MyClass.hello();
+        }
+    }
+}
